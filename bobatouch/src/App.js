0ceda77371470e945887cdb72ed7c7bfab007712
Danny Nguyen <dang0781@colorado.edu>
2020-02-03 16:48:31 -0700

    Initial React App setup

---
+++ bobatouch/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Boba Touch
+        </h1>
+        <h2>
+          Coming Soon
+        </h2>
+        <p>
+          Summer 2020
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
