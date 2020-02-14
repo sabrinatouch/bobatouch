b9675f5ab0c33bd0e1309e610278f89a87028cdc
Gabe <36547609+gaberaymondi@users.noreply.github.com>
2020-02-14 14:03:46 -0700

    Wire Frame

---
+++ iOS/boba_touch/boba_touch/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  boba_touch
+//
+//  Created by Gabe Raymondi on 2/14/20.
+//  Copyright © 2020 Gabe Raymondi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
