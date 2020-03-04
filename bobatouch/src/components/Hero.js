49ffc48395078f9d7a4ae80b93f3bdf7e0348593
= <matthewcb11@gmail.com>
2020-03-04 14:03:00 -0700

    hero template

---
+++ bobatouch/src/components/Hero.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import "./Hero.css"
+
+export default class Hero extends Component {
+    render() {
+        return (
+            <div class="hero-image">
+                <div class="hero-text">
+                    <div>BOBA TOUCH</div>
+                    
+                    <div>coming soon</div>
+                </div>
+            </div>
+        )
+    }
+}
