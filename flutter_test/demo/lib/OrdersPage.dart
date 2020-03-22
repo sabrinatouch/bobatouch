11cc0928b0ff6e1d8af3cbcbfb584b7b3f45e18a
TwoChiangz <32112539+TwoChiangz@users.noreply.github.com>
2020-03-21 18:21:43 -0600

    Mirrors the menu items
    Made the format to be closer to what the menu will look like

--- flutter_test/demo/lib/OrdersPage.dart
+++ flutter_test/demo/lib/OrdersPage.dart
@@ -9,10 +9,18 @@ class _OrdersPageState extends State<OrdersPage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body:
-      CustomScrollView(
+      body: CustomScrollView(
         primary: false,
         slivers: <Widget>[
+          SliverAppBar(
+            title: Text('Milk Tea',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.black,
+                )
+            ),
+            backgroundColor: Colors.white,
+          ),
           SliverPadding(
             padding: const EdgeInsets.all(20),
             sliver: SliverGrid.count(
@@ -20,12 +28,98 @@ class _OrdersPageState extends State<OrdersPage> {
               mainAxisSpacing: 10,
               crossAxisCount: 2,
               children: <Widget>[
-                _buildMenuItem('assets/drink1.jpg', 'Item 1'),
-                _buildMenuItem('assets/drink2.jpg', 'Item 2'),
-                _buildMenuItem('assets/drink3.jpg', 'Item 3'),
-                _buildMenuItem('assets/drink1.jpg', 'Item 4'),
-                _buildMenuItem('assets/drink2.jpg', 'Item 5'),
-                _buildMenuItem('assets/drink3.jpg', 'Item 6'),
+                _buildMenuItem('assets/drink1.jpg', 'Earl Grey Milk Tea'),
+                _buildMenuItem('assets/drink2.jpg', 'Assam Milk Tea'),
+                _buildMenuItem('assets/drink3.jpg', 'Thai Milk Tea'),
+                _buildMenuItem('assets/drink1.jpg', 'Coconut Milk Tea'),
+                _buildMenuItem('assets/drink2.jpg', 'Match Milk Tea'),
+              ],
+            ),
+          ),
+          SliverAppBar(
+            title: Text('Coffee',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.black,
+                )
+            ),
+            backgroundColor: Colors.white,
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(20),
+            sliver: SliverGrid.count(
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 2,
+              children: <Widget>[
+                _buildMenuItem('assets/drink1.jpg', 'Cold Brew Hazelnut Vietnamese Coffee'),
+              ],
+            ),
+          ),
+          SliverAppBar(
+            title: Text('Fruity',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.black,
+                )
+            ),
+            backgroundColor: Colors.white,
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(20),
+            sliver: SliverGrid.count(
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 2,
+              children: <Widget>[
+                _buildMenuItem('assets/drink1.jpg', 'Hibiscus'),
+                _buildMenuItem('assets/drink2.jpg', 'Lemon Grass'),
+                _buildMenuItem('assets/drink3.jpg', 'Giner'),
+                _buildMenuItem('assets/drink1.jpg', 'Lemonade'),
+              ],
+            ),
+          ),
+          SliverAppBar(
+            title: Text('Flavored Milk',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.black,
+                )
+            ),
+            backgroundColor: Colors.white,
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(20),
+            sliver: SliverGrid.count(
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 2,
+              children: <Widget>[
+                _buildMenuItem('assets/drink1.jpg', 'Strawberry Milk'),
+                _buildMenuItem('assets/drink2.jpg', 'Ube Milk'),
+                _buildMenuItem('assets/drink3.jpg', 'Mung Bean Pandan Milk'),
+              ],
+            ),
+          ),
+          SliverAppBar(
+            title: Text('Ice Cream',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.black,
+                )
+            ),
+            backgroundColor: Colors.white,
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(20),
+            sliver: SliverGrid.count(
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 2,
+              children: <Widget>[
+                _buildMenuItem('assets/drink1.jpg', 'Brown Sugar Milk Tea'),
+                _buildMenuItem('assets/drink2.jpg', 'Thai Iced Tea'),
+                _buildMenuItem('assets/drink3.jpg', 'Matcha Milk Tea'),
               ],
             ),
           ),
@@ -37,7 +131,7 @@ class _OrdersPageState extends State<OrdersPage> {
 
 _buildMenuItem(String imgPath, String name) {
   return Container(
-    padding: const EdgeInsets.all(8),
+      padding: const EdgeInsets.all(8),
       child: Stack(
         children: <Widget>[
           // Stroked text as border.
